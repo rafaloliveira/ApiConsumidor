5b4487df97a086f09bd5d838b8ba48754ca30674
rafaeloliveira <eurafaelmedeiros@gmail.com>
2022-04-14 18:38:47 -0300

    ddd

--- consumidorfront/src/App.js
+++ consumidorfront/src/App.js
@@ -3,27 +3,38 @@ import "./App.css";
 
 function App() {
   const [data, setData] = useState([]);
+  const [art, setArt] = useState([]);
+  const artigos = art.artigos;
+  console.log(artigos);
+  console.log(art);
   console.log(data);
 
   useEffect(() => {
     fetch("http://localhost:8080/")
       .then((response) => response.json())
+
       .then((response) => setData(response));
+  }, []);
+  useEffect(() => {
+    fetch("http://localhost:8080/")
+      .then((response) => response.json())
+
+      .then((response) => setArt(response));
   }, []);
   return (
     <div className="App">
       <div className="fundo">
         <h1>
-          Código de Defesa do Consumidor - Lei 8078/90 | Lei nº 8.078, de 11 de
-          setembro de 1990
+          <center>
+            Código de Defesa do Consumidor{" "}
+            <p>Lei nº 8.078, de 11 de setembro de 1990</p>
+          </center>
         </h1>
         <div>
-          {" "}
           {data &&
             data.map((d) => (
-              <div key={d.artigo}>
-                <li>{d.artigo}</li>
-                <li>{d.paragrafo}</li>
+              <div key={d.artigos}>
+                <p>{d.capitulo}</p>
               </div>
             ))}
         </div>
